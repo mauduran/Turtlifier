b0df2d96e5d72de59f7077c835b46fa63dc794d0
JPRR44 <juanp.robles@hotmail.com>
2021-12-06 00:30:36 +0100

    Feat: Added date and names as comments

--- turtl-front/turtl-front/src/setupTests.js
+++ turtl-front/turtl-front/src/setupTests.js
@@ -1,3 +1,14 @@
+/*
+############################################
+############################################
+###     Date: December 7, 2021           ###
+###     @author Mauricio Duran Padilla   ###
+###     @author Juan Pablo Ramos Robles  ###
+###     @author Alfonso Ramírez Casto    ###
+############################################
+############################################
+*/
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
--- turtlifier/turtlifier/config.py
+++ turtlifier/turtlifier/config.py
@@ -1,3 +1,13 @@
+
+############################################
+############################################
+###     Date: December 7, 2021           ###
+###     @author Mauricio Duran Padilla   ###
+###     @author Juan Pablo Ramos Robles  ###
+###     @author Alfonso Ramírez Casto    ###
+############################################
+############################################
+
 import configparser
 
 class Config:
