94ba85eb679f74f3c2b09e8176182188ced64966
mauduran <is716122@iteso.mx>
2021-12-05 16:53:13 +0100

    Tweaked turtlify method, added separator and prefixes as param and changed graph structure

--- turtlifier/turtlifier/converter.py
+++ turtlifier/turtlifier/converter.py
@@ -1,61 +1,63 @@
 import csv
-from rdflib import Graph, Literal, RDF, URIRef, Namespace #basic RDF handling
+from rdflib import Graph, Literal, RDF, URIRef, Namespace  # basic RDF handling
 
 
 class Converter:
-
-    #Transform a string to upper camel case notation
+    @staticmethod
+    # Transform a string to upper camel case notation
     def to_upper_camel_case(text) -> str:
-         # s = text.replace("-", " ").replace("_", " ")
         s = text.split()
         if len(text) == 0:
             return text
         return ''.join(i.capitalize() for i in s[0:])
 
-    #Transform a string to lower camel case notation
+    @staticmethod
+    # Transform a string to lower camel case notation
     def to_lower_camel_case(text) -> str:
-         # s = text.replace("-", " ").replace("_", " ")
         s = text.split()
         if len(text) == 0:
             return text
         return s[0] + ''.join(i.capitalize() for i in s[1:])
 
-    #receives a string and returns it turtlified 
-    def turtlify(file_text)-> str:
-        
-        #create graph to store the RDF triples
+    # receives a string and returns it turtlified
+    def turtlify(
+        file_text, 
+        separator=",", 
+        dataPrefix=("data",'http://example.org/data/'), #Prefix name and uri come as a tuple
+        predPrefix=("pred",'http://example.org/predicate/'), #Prefix name and uri come as a tuple
+        ) -> str:
+
+        # create graph to store the RDF triples
         g = Graph()
+
+        # prefixes to use
+        dataPrefNs = Namespace(dataPrefix[1])
+        predicatePrefNs = Namespace(predPrefix[1])
         
-        #TODO use the prefixes that the user submitted, or use the default ones
-        #prefixes to use
-        data = Namespace('http://example.org/data/')
-        predicate = Namespace('http://example.org/predicate/')
+        # Binding Prefixes
+        g.bind(dataPrefix[0], dataPrefNs)
+        g.bind(predPrefix[0], predicatePrefNs)
 
 
-        #TODO use the separator that the user submitted, or use the default one
-        #Parse the file_text to a csv dictionary
-        csvreader = csv.DictReader(file_text,quotechar='"',delimiter=';')
+        csvreader = csv.DictReader(
+            file_text, quotechar='"', delimiter=separator)
 
         # if the first row contains header information, retrieve it like so
         headers = csvreader.fieldnames
+        
 
-        for row in csvreader:
-            #row is an array of the columns in the file
-            #Make sure to check the encoding of the strings in the array
-            
-            #The subject will always be the same for each turtle triple. We say that the <subject> <is a> <"whatever the header says">
-            #According to the turtle rules, the subject - row[headers[0]] - needs to be in upper camel case. The object should be in upper camel case as well, but we are treating it as a Literal
-            g.add((URIRef(data+Converter.to_upper_camel_case(row[headers[0]])), RDF.type, Literal(headers[0])))
-           
-            #add the rest of values for the turtle triple according to the available headers
-            #Since the subject is still the same, we are only adding more predicates and objects to the turtle triple.
-            #According to the turtle rules, the subject needs to be in upper camel case, and the predicate in lower camel case. We are still treating the object as a Literal, so none of the camelcases will apply
-            for i in range(1,len(headers)):
-                g.add((URIRef(data+Converter.to_upper_camel_case(row[headers[0]])), URIRef(predicate+Converter.to_lower_camel_case(headers[i])), Literal(row[headers[i]])))
-        # print(g.serialize(format='turtle'))
+        # row is an array of the columns in the file, idx refers to the line number
+        for (idx, row) in enumerate(csvreader):
+            # Make sure to check the encoding of the strings in the array
+
+            # Take index or line number as the definition of the URI and main subject
+            # Proceed to add  predicates and objects to the turtle triple.
+            # According to the turtle rules, the subject needs to be in upper camel case, and the predicate in lower camel case. We are still treating the object as a Literal, so none of the camelcases will apply
+            for i in range(0, len(headers)):
+                subj = dataPrefNs["subj" + str(idx)]
+                g.add((
+                    URIRef(subj), 
+                    URIRef(predicatePrefNs[Converter.to_lower_camel_case(headers[i])]),
+                    Literal(row[headers[i]])
+                ))
         return g.serialize(format='turtle')
-
-
-
-
-
